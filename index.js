d68dd961fcf8e5b9165539a7defcf0552472fb22
Wanda Larosiliere <wglarosiliere@gmail.com>
2020-07-13 04:41:15 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,42 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+console.log(string.toLowerCase())
+}
+
+
+ function sayHiToGrandma(string) 
+{
+  if (string === string.toLowerCase()) 
+  {return ("I can't hear you")} 
+  else if (string === string.toUpperCase())
+  {return ("YRS INDEED")}
+  
+}
+
+
+if (dog === "hungry") {
+  console.log("Refilling food bowl.")
+ 
+} else if (dog === "thirsty") {
+  console.log("Refilling water bowl.")
+ 
+} else if (dog === "playful") {
+  console.log("Playing tug-of-war.")
+ 
+} else if (dog === "cuddly") {
+  console.log("Snuggling.")
+ 
+} else { 
+  console.log("Reading newspaper.")
+

593973c3ec6585dd00e50d44128dd8dd5dc5e6fa
Wanda Larosiliere <wglarosiliere@gmail.com>
2020-07-13 04:49:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,18 +28,3 @@ console.log(string.toLowerCase())
 }
 
 
-if (dog === "hungry") {
-  console.log("Refilling food bowl.")
- 
-} else if (dog === "thirsty") {
-  console.log("Refilling water bowl.")
- 
-} else if (dog === "playful") {
-  console.log("Playing tug-of-war.")
- 
-} else if (dog === "cuddly") {
-  console.log("Snuggling.")
- 
-} else { 
-  console.log("Reading newspaper.")
-
